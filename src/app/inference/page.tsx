30725eb78a1a77383379d34d9543fdaa4d11da72
garvsl <garvsehgal7@gmail.com>
2024-10-26 14:47:06 -0400

    webcam setup

--- src/app/inference/page.tsx
+++ src/app/inference/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { useCallback, useRef } from "react";
+import Webcam from "react-webcam";
+
 export default function Inference() {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user",
+  };
+
+  const webcamRef = useRef(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+  }, [webcamRef]);
+
+  const video = webcamRef.current.video;
   return (
-    <div>
-      <h1>inference</h1>
-    </div>
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+    </>
   );
 }
