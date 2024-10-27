e5550f1b4eda0e853545ec622122d9b8f8252f6c
birongliu <birongliu2003@gmail.com>
2024-10-27 10:26:37 -0400

    Remove unused code and console.log statements

--- src/app/lib/auth-action.ts
+++ src/app/lib/auth-action.ts
@@ -55,7 +55,6 @@ export async function signup(formData: FormData) {
 export async function signout() {
   const supabase = createClient();
   const { error } = await supabase.auth.signOut();
-  console.log(error)
   if (error) {
     console.log(error);
     redirect("/error");
@@ -64,24 +63,12 @@ export async function signout() {
   redirect("/");
 }
 
-const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/'
-  // Make sure to include `https://` when not localhost.
-  url = url.startsWith('http') ? url : `https://${url}`
-  // Make sure to include a trailing `/`.
-  url = url.endsWith('/') ? `${url}` : `${url}/`
-  return url
-}
-
 export async function signInWithGoogle() {
   const supabase = createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${getURL()}learn`,
+      redirectTo: process.env.NEXT_PUBLIC_REDIRECT_URL,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
