e1f685965b4759e280e4e2b6c3e0e4db0db50691
garvsl <garvsehgal7@gmail.com>
2024-10-27 02:23:21 -0400

    font attempt

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,26 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         // Core Colors
-        'feather-green': '#58CC02',  // Feather Green
-        'mask-green': '#89E219',     // Mask Green
-        'eel': '#4B4B4B',            // Eel
-        'snow': '#FFFFFF',           // Snow
+        "feather-green": "#58CC02", // Feather Green
+        "mask-green": "#89E219", // Mask Green
+        eel: "#4B4B4B", // Eel
+        snow: "#FFFFFF", // Snow
 
         // Secondary Colors
-        'macaw': '#1CB0F6',          // Macaw
-        'cardinal': '#FF4B4B',       // Cardinal
-        'bee': '#FFC800',            // Bee
-        'fox': '#FF9600',            // Fox
-        'beetle': '#CE82FF',         // Beetle
-        'humpback': '#2B70C9',       // Humpback
+        macaw: "#1CB0F6", // Macaw
+        cardinal: "#FF4B4B", // Cardinal
+        bee: "#FFC800", // Bee
+        fox: "#FF9600", // Fox
+        beetle: "#CE82FF", // Beetle
+        humpback: "#2B70C9", // Humpback
 
         // Neutral Colors
-        'wolf': '#777777',           // Wolf
-        'hare': '#AFAFAF',           // Hare
-        'swan': '#E5E5E5',           // Swan
-        'polar': '#F7F7F7',          // Polar
+        wolf: "#777777", // Wolf
+        hare: "#AFAFAF", // Hare
+        swan: "#E5E5E5", // Swan
+        polar: "#F7F7F7", // Polar
       },
     },
   },
