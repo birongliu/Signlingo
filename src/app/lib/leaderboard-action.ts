ec6bf52583c2978c7bb9190f13c274fc7f1ec2b5
birongliu <birongliu2003@gmail.com>
2024-10-27 09:55:03 -0400

    Refactor leaderboard-action.ts to handle profile lessons and generate leaderboard

--- src/app/lib/leaderboard-action.ts
+++ src/app/lib/leaderboard-action.ts
@@ -2,35 +2,37 @@
 import { createClient } from "@/app/utils/supabase/server";
 
 export interface LeaderBoard {
-    id: string;
-    username: string;
-    lesson_completed: number;
+  id: string;
+  username: string;
+  lesson_completed: number;
 }
 
 export const getleaderboard = async (): Promise<LeaderBoard[]> => {
   const results: LeaderBoard[] = [];
   const supabase = createClient();
   const { data, error } = await supabase
-  .from('profiles')
-  .select(`
-    id, full_name,
-    profile_lessons (
-      lesson_id
+    .from("profiles")
+    .select(
+      `
+      id, 
+      full_name,
+      profile_lessons(lesson_id)
+    `,
     )
-  `)
+    .order("full_name", { ascending: true });
+  console.log(data);
   if (error) {
     console.error(error);
     return [];
   }
 
   data.map((user) => {
-
     results.push({
       id: user.id,
       username: user.full_name,
-      lesson_completed: user.profile_lessons.length,
+      lesson_completed: user.profile_lessons ? user.profile_lessons.length : 0,
     });
   });
   console.log(results);
   return results;
-}
+};
