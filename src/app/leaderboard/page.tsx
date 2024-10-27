630149bd89f7a33d4dba38e351a4932736bd57a9
wilson <wilsonliu202@gmail.com>
2024-10-26 21:51:18 -0400

    upload leaderboard

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -29,24 +29,22 @@ export default function LeaderboardPage() {
 
       {/* Main Content */}
       <main className="flex-1 p-6">
-        <h1 className="mb-6 text-center text-3xl font-bold text-gray-800">
+        <h1 className="text-eel mb-6 text-center text-3xl font-bold">
           Leaderboard
         </h1>
 
         <section className="mb-8">
-          <h2 className="mb-4 text-2xl font-semibold text-gray-700">
-            Top Learners
-          </h2>
+          <h2 className="text-eel mb-4 text-2xl font-semibold">Top Learners</h2>
           <div className="flex flex-col gap-3">
             {sortedLeaderboard.map((user, index) => (
               <div
                 key={index}
-                className="flex justify-between rounded-lg bg-lime-100 p-4 shadow-md hover:bg-lime-200"
+                className="bg-mask-green hover:bg-feather-green flex justify-between rounded-lg p-4 shadow-md"
               >
-                <span className="text-xl font-bold text-gray-800">
+                <span className="text-xl font-bold text-white">
                   {index + 1}. {user.name}
                 </span>
-                <span className="text-xl font-bold text-gray-600">
+                <span className="text-xl font-bold text-white">
                   {user.lessonsCompleted} Lessons Completed
                 </span>
               </div>
